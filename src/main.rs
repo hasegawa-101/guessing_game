9459658f12daecee6815a0a488bd8103519a8e3d
hasegawa-101 <hasegawa@101de-sign.com>
2022-06-19 17:16:28 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // 1から100までの乱数を生成
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);     // 次のように予想しました: {}
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),       //小さすぎ！
+            Ordering::Greater => println!("Too big!"),      //大きすぎ！
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
